e9dde98dc4e4af654a6064b900468ce32f144832
Daniel Alemu <dsa.alemu@gmail.com>
2021-06-15 13:49:07 -0400

    updated lab1

---
+++ Makefile
@@ -0,0 +1,8 @@
+# simple Makefile
+# Daniel Alemu 00884794
+#  CS 262, Lab Section 207
+#  Lab 1
+all: lab1_dalemu2_207.c
+	gcc -g -Wall -o lab1_dalemu2_207 lab1_dalemu2_207.c
+clean:
+	rm lab1_dalemu2_207
---
+++ lab1_dalemu2_207.c
@@ -0,0 +1,13 @@
+/*Daniel Alemu 00884798
+ * CS 262, Lab Section 207
+ *  * Lab 1
+ *   */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("My name is Daniel Alemu .\n");
+    return 0;
+}
